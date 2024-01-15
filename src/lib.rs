f1de02669eed75403c9db4785dc43d57658bcc21
xerinox <rkildal@gmail.com>
2024-01-15 19:42:50 +0100

    Add multithreading, locked behind multithreaded feature flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
-use serde::Serialize;
-use std::{fs, path::PathBuf, str::FromStr};
 use clap::Parser;
+use serde::Serialize;
+use std::{
+    fs,
+    path::PathBuf,
+    str::FromStr,
+};
+#[cfg(feature="multithreaded")]
+use std::sync::{mpsc, Arc, Mutex};
+#[cfg(feature="multithreaded")]
+use std::thread;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -22,7 +30,12 @@ pub struct Arguments {
     pub content: Option<String>,
 
     #[arg(short, long, default_value_t = String::from("/"))]
-    pub endpoint: String
+    pub endpoint: String,
+
+    #[cfg(feature="multithreaded")]
+    #[arg(short, long, default_value_t = 1)]
+    #[cfg(feature="multithreaded")]
+    pub threads: usize,
 }
 
 #[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
@@ -48,14 +61,15 @@ impl FromStr for ResponseFormat {
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 pub struct Response {
     pub content: Option<String>,
     pub format: ResponseFormat,
-    pub endpoint: String
+    pub endpoint: String,
 }
 
 impl ToString for Response {
+    /// Turns response into http response string
     fn to_string(&self) -> String {
         let mut response = String::from("HTTP/1.1 200 OK\r\n");
         match &self.format {
@@ -66,9 +80,9 @@ impl ToString for Response {
                 response.push_str("Content-Type: application/json\r\n");
             }
         }
-        let content = match &self.content  {
+        let content = match &self.content {
             Some(content) => content,
-            None => ""
+            None => "",
         };
         response.push_str("Content-Length: ");
         response.push_str(&content.len().to_string());
@@ -80,48 +94,143 @@ impl ToString for Response {
 }
 
 impl Response {
-    pub fn from_content(content: &str, endpoint: &str, response_format: &ResponseFormat) -> Response {
+    /// Creates a response from text content, endpoint, and format
+    pub fn from_content(
+        content: &str,
+        endpoint: &str,
+        response_format: &ResponseFormat,
+    ) -> Response {
         Response {
             content: Some(String::from(content)),
             format: response_format.clone(),
-            endpoint: String::from(endpoint)
+            endpoint: String::from(endpoint),
         }
     }
 
+    /// Creates a response from content file and response format
     pub fn from_content_file(path: &PathBuf, response_format: &ResponseFormat) -> Response {
         Response {
             content: match path.exists() {
                 true => Some(fs::read_to_string(path).expect("File is unreadable")),
                 false => {
-                    println!("Could not find file: {:}, continuing with blank response", path.to_str().expect("Path is unparseable"));
+                    println!(
+                        "Could not find file: {:}, continuing with blank response",
+                        path.to_str().expect("Path is unparseable")
+                    );
                     None
-                },
+                }
             },
             format: response_format.clone(),
             endpoint: match path.file_stem() {
                 Some(path) => {
                     let mut endpoint: String = String::from("/");
-                        endpoint.push_str(&path.to_string_lossy());
-                        endpoint
-                    
-                },
-                None => String::from("/")
+                    endpoint.push_str(&path.to_string_lossy());
+                    endpoint
+                }
+                None => String::from("/"),
             },
         }
     }
 
+    /// Creates a response from Argument object
     pub fn from_args(args: &Arguments) -> Response {
         let response_format = &args.format;
         if let Some(content) = &args.content {
-            return Response::from_content(content, &args.endpoint, response_format)
+            return Response::from_content(content, &args.endpoint, response_format);
         } else if let Some(p) = &args.content_file {
-            return Response::from_content_file(p, response_format) 
+            return Response::from_content_file(p, response_format);
         } else {
             return Response {
                 content: None,
                 format: response_format.clone(),
-                endpoint: String::from("/")
+                endpoint: String::from("/"),
+            };
+        }
+    }
+}
+
+#[cfg(feature="multithreaded")]
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+#[cfg(feature="multithreaded")]
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+#[cfg(feature="multithreaded")]
+pub struct PoolCreationError {}
+
+#[cfg(feature="multithreaded")]
+impl ThreadPool {
+    /// Builds a thread pool
+    ///
+    /// size is how many threads to spawn
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size < 1 {
+            Err(PoolCreationError {})
+        } else {
+            let (sender, receiver) = mpsc::channel();
+            let receiver = Arc::new(Mutex::new(receiver));
+            let mut workers = Vec::with_capacity(size);
+            for id in 0..size {
+                workers.push(Worker::new(id, Arc::clone(&receiver)));
             }
+            Ok(ThreadPool {
+                workers,
+                sender: Some(sender),
+            })
+        }
+    }
+    /// Executes a job
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+#[cfg(feature="multithreaded")]
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
+#[cfg(feature="multithreaded")]
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+#[cfg(feature="multithreaded")]
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
+        });
+        Worker {
+            id,
+            thread: Some(thread),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::io::BufReader;
-use std::io::prelude::*;
 use clap::Parser;
+use std::io::prelude::*;
+use std::io::BufReader;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use testsuite::{Response, Arguments};
-
+#[cfg(feature = "multithreaded")]
+use testsuite::ThreadPool;
+use testsuite::{Arguments, Response};
 
 fn main() {
     let args = Arguments::parse();
@@ -16,26 +17,66 @@ fn main() {
     let end_point: String = HOST.to_owned() + ":" + &port.to_string();
     println!("Server running on:{:}{:}", end_point, response.endpoint);
     let listener = TcpListener::bind(end_point).unwrap();
+    #[cfg(feature = "multithreaded")]
+    if args.threads > 1 {
+        let pool = ThreadPool::build(args.threads);
+        match &pool {
+            Ok(pool) => {
+                for stream in listener.incoming() {
+                    let _stream = stream.unwrap();
+                    let resp = response.clone();
+                    pool.execute(move || {
+                        handle_connection(_stream, &resp);
+                    });
+                }
+            }
+            Err(_) => {
+                println!("Could not initalize pool, running single threaded");
+                for stream in listener.incoming() {
+                    let _stream = stream.unwrap();
+                    handle_connection(_stream, &response);
+                }
+            }
+        }
+    } else {
+        for stream in listener.incoming() {
+            let _stream = stream.unwrap();
+            handle_connection(_stream, &response);
+        }
+    }
+    #[cfg(not(feature = "multithreaded"))]
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
         handle_connection(_stream, &response);
-
-        println!("Connection established!");
     }
 }
 
 fn handle_connection(mut stream: TcpStream, response: &Response) {
-    let mut line = vec![];
-    let mut reader = BufReader::new(stream.try_clone().unwrap());
-    if reader.read_until(b'\n', &mut line).unwrap() > 0 {
-        let string_line= String::from_utf8(line).expect("error in stream read");
-        let path = string_line.trim_start_matches("GET ").trim_start_matches("POST ").trim_end_matches(" HTTP/1.1\r\n");
-        if path == response.endpoint {
-            stream.write(response.to_string().as_bytes()).unwrap();
-            println!("Matched path: {}, responded with: {:?}", path, response.to_string());
-        } else {
-            println!("Unmatched path: {}, expected: {}", path, response.endpoint);
-        }
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let string_line = http_request.first().unwrap();
+    let path = string_line
+        .trim_start_matches("GET ")
+        .trim_start_matches("POST ")
+        .trim_end_matches(" HTTP/1.1");
+
+    if path == response.endpoint {
+        stream.write(response.to_string().as_bytes()).unwrap();
+        println!(
+            "Matched path: {}, responded with: {:?}",
+            path,
+            response.to_string()
+        );
+    } else {
+        println!("Unmatched path: {}, expected: {}", path, response.endpoint);
+        stream
+            .write("HTTP/1.1 404 Not Found\r\n\r\nNot found".as_bytes())
+            .unwrap();
     }
     stream.flush().unwrap();
 }
